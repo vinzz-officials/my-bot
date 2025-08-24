32c1e64af1257bbe13d15ce7c33f8b48d542a42c
vinzz-officials <vinzz4358@gmail.com>
2025-08-25 01:43:35 +0800

    Update bot.js

--- api/bot.js
+++ api/bot.js
@@ -1,4 +1,4 @@
-default async function handler(req, res) {
+export default async function handler(req, res) {
   // === CONFIG ===
   const TOKEN = process.env.TG_TOKEN || "8396430373:AAGZ9lbLgPhAhIZUghflXgYls1taRpmPudY"; // disarankan: gunakan env var TG_TOKEN
   const OWNER_NAME = "Vinzz Official";
